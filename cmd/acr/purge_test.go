827c7d6127e8f908ae0db8353be4740f8944b939
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-09-18 12:07:39 +0100

    fix typo

--- cmd/acr/purge_test.go
+++ cmd/acr/purge_test.go
@@ -709,7 +709,7 @@ var (
 		Response: autorest.Response{
 			Response: &http.Response{
 				StatusCode: 200,
-				Header:     http.Header{linkHeader: {"/acr/v1/&testRepo/_tags?last=123%26latest&n=3&orderby=timedesc rel=\"next\""}},
+				Header:     http.Header{linkHeader: {"/acr/v1/&testRepo/_tags?n=3&orderby=timedesc&last=123%26latest; rel=\"next\""}},
 			},
 		},
 		Registry:  &testLoginURL,
