31f24f06419ed6d9d89f25b0166a75dc9a03b3ca
wangxiaoxuan273 <wangxiaoxuan119@gmail.com>
2023-03-27 10:37:25 +0000

    test: add unit tests for GetAcrCLIClientWithAuth
    Signed-off-by: wangxiaoxuan273 <wangxiaoxuan119@gmail.com>

--- cmd/api/acrsdk_test.go
+++ cmd/api/acrsdk_test.go
@@ -4,12 +4,16 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"reflect"
 	"testing"
 
 	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
 )
 
 func TestLoginURLWithPrefix(t *testing.T) {
@@ -67,23 +71,70 @@ func TestGetExpiration(t *testing.T) {
 }
 
 func TestGetAcrCLIClientWithAuth(t *testing.T) {
-	loginURL := "myregistry.azurecr.io"
+	var testLoginURL string
+	testTokenScope := "registry:catalog:* repository:*:*"
+	testAccessToken := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDk2NTEwMzEsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyfQ.JamFfuEx54E0ZqGFx8EUe745GisoP0YCTAup-8YB2Tb4nouKfZGBbHb-YRtEqi-r8uQzihJBRi1GDxUNzDxbsLB5fe8Q8uLN8IpTHmWIMGpoD7E9W5W8-hUJiu4qG6lPZMV7e0JDsik-xUPcFjv8yJQmKl84vwDFRpH0uNhBDs_H6YnIGyso1I5ITkeSiUVpdlkeOUQ1wK-k-rAU8zYUSCj_3ke0zHQcHFXRiHsmyUMZBKNmPUTZp1CSBWx90zKGBmhgt26LHl1EmmoddRbuigReOqT_HnaTcTrrudweLPXubjHMtAFym5RDzGsIae4mkClKV6iMVweM73A2NWQNkg"
+	testRefreshToken := "test/refresh/token"
+
+	// create an authorization server
+	as := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusNotFound)
+			panic("unexpected access")
+		}
+		switch path {
+		case "/oauth2/token":
+			if err := r.ParseForm(); err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				panic("failed to parse form")
+			}
+			if got := r.PostForm.Get("service"); got != testLoginURL {
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+			// handles refresh token requests
+			if got := r.PostForm.Get("grant_type"); got != "refresh_token" {
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+			if got := r.PostForm.Get("scope"); got != testTokenScope {
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+			if got := r.PostForm.Get("refresh_token"); got != testRefreshToken {
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+			// writes back access token
+			if _, err := fmt.Fprintf(w, `{"access_token":%q}`, testAccessToken); err != nil {
+				panic(err)
+			}
+		default:
+			w.WriteHeader(http.StatusNotAcceptable)
+		}
+	}))
+	defer as.Close()
+	testLoginURL = as.URL
+	sender := autorest.CreateSender()
+	sender.(*http.Client).Transport = as.Client().Transport
+
 	dockerConfigWithUsernameAndPassword := []byte(`{"auths":{"myregistry.azurecr.io":{"auth":"aGVsbG86b3Jhcy10ZXN0"}}}`)
-	// dockerConfigNoUsername := []byte(`{"auths":{"myregistry.azurecr.io":{"auth":"MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwOg==","identitytoken":"test refresh token"}}}`)
+	dockerConfigNoUsername := []byte(fmt.Sprintf(`{"auths":{"%s":{"auth":"MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwOg==","identitytoken":"test/refresh/token"}}}`, testLoginURL))
 	dockerConfigWithUsernameOnly := []byte(`{"auths":{"myregistry.azurecr.io":{"auth":"aGVsbG86"}}}`)
+
 	tests := []struct {
-		name           string
-		username       string
-		password       string
-		configContent  []byte
-		wantErr        bool
-		useBasicAuth   bool
-		wantUsername   string
-		wantPassword   string
-		useBearerToken bool
+		name             string
+		loginURL         string
+		username         string
+		password         string
+		configContent    []byte
+		wantErr          bool
+		useBasicAuth     bool
+		wantUsername     string
+		wantPassword     string
+		wantAccessToken  string
+		wantRefreshToken string
 	}{
 		{
 			name:          "empty username and password, read from docker config with regular username and password",
+			loginURL:      "myregistry.azurecr.io",
 			username:      "",
 			password:      "",
 			configContent: dockerConfigWithUsernameAndPassword,
@@ -92,37 +143,45 @@ func TestGetAcrCLIClientWithAuth(t *testing.T) {
 			wantUsername:  "hello",
 			wantPassword:  "oras-test",
 		},
-		// To be continue
-		// {
-		// 	name:          "empty username and password, read from docker config with username 00000000-0000-0000-0000-000000000000",
-		// 	username:      "",
-		// 	password:      "",
-		// 	configContent: dockerConfigNoUsername,
-		// 	wantErr:       false,
-		// },
+		{
+			name:             "empty username and password, read from docker config with username 00000000-0000-0000-0000-000000000000",
+			loginURL:         testLoginURL,
+			username:         "",
+			password:         "",
+			configContent:    dockerConfigNoUsername,
+			wantErr:          false,
+			useBasicAuth:     false,
+			wantAccessToken:  testAccessToken,
+			wantRefreshToken: testRefreshToken,
+		},
 		{
 			name:     "password is empty, fail with an error",
+			loginURL: "myregistry.azurecr.io",
 			username: "test user",
 			password: "",
 			wantErr:  true,
 		},
 		{
 			name:          "empty username and password, read from docker config with username only, fail with an error",
+			loginURL:      "myregistry.azurecr.io",
 			username:      "",
 			password:      "",
 			configContent: dockerConfigWithUsernameOnly,
 			wantErr:       true,
 		},
-		// To be continue
-		// {
-		// 	name:         "empty username, refresh token as password",
-		// 	username:     "",
-		// 	password:     "test refresh token",
-		// 	wantErr:      false,
-		// 	useBasicAuth: false,
-		// },
+		{
+			name:             "empty username, refresh token as password",
+			loginURL:         testLoginURL,
+			username:         "",
+			password:         testRefreshToken,
+			wantErr:          false,
+			useBasicAuth:     false,
+			wantAccessToken:  testAccessToken,
+			wantRefreshToken: testRefreshToken,
+		},
 		{
 			name:         "non-empty username and password",
+			loginURL:     "myregistry.azurecr.io",
 			username:     "hello",
 			password:     "oras-test",
 			wantErr:      false,
@@ -139,13 +198,21 @@ func TestGetAcrCLIClientWithAuth(t *testing.T) {
 				t.Errorf("cannot create test config file: %v", err)
 				return
 			}
-			got, err := GetAcrCLIClientWithAuth(loginURL, tt.username, tt.password, []string{configFilePath})
+			got, err := GetAcrCLIClientWithAuth(tt.loginURL, tt.username, tt.password, []string{configFilePath})
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetAcrCLIClientWithAuth() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if tt.useBasicAuth {
-				wantAuthorizer := autorest.NewBasicAuthorizer(tt.wantUsername, tt.wantPassword)
+			if !tt.wantErr {
+				var wantAuthorizer autorest.Authorizer
+				if tt.useBasicAuth {
+					wantAuthorizer = autorest.NewBasicAuthorizer(tt.wantUsername, tt.wantPassword)
+				} else {
+					wantAuthorizer = autorest.NewBearerAuthorizer(&adal.Token{
+						AccessToken:  tt.wantAccessToken,
+						RefreshToken: tt.wantRefreshToken,
+					})
+				}
 				if !reflect.DeepEqual(got.AutorestClient.Authorizer, wantAuthorizer) {
 					t.Error("incorrect AutorestClient.Authorizer")
 				}
